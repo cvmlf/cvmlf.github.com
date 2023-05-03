b6c3bd08d7559e990d06fe4349e4682d51f7af70
Tony Eichelberger <tony.eichelberger@jamf.com>
2023-05-02 21:00:02 -0500

    fixed up the live timer to be smarter

--- js/mlf.js
+++ js/mlf.js
@@ -5,8 +5,8 @@
  */
 
 // jQuery for page scrolling feature - requires jQuery Easing plugin
-$(function() {
-    $('.page-scroll a').bind('click', function(event) {
+$(function () {
+    $('.page-scroll a').bind('click', function (event) {
         var $anchor = $(this);
         $('html, body').stop().animate({
             scrollTop: $($anchor.attr('href')).offset().top
@@ -21,22 +21,30 @@ $('body').scrollspy({
 })
 
 // Closes the Responsive Menu on Menu Item Click
-$('.navbar-collapse ul li a').click(function() {
+$('.navbar-collapse ul li a').click(function () {
     $('.navbar-toggle:visible').click();
 });
 
 
 
 // load the live results from the website
-$(document).ready(function() {
-  $.ajax({
-    //url: "http://localhost:3000/live",
-    url: "https://cvmlf.herokuapp.com/live",
-    cache: false
-  })
-  .done(function( html ) {
-    $( "#live-results" ).append( html );
-    $( "#normal-header" ).hide();
-  });
+$(document).ready(function () {
+  checkLive();
 });
+
+let timerId = setInterval(() => checkLive(), 1000*30);
+
+function checkLive() {
+    $.ajax({
+        //url: "http://localhost:3000/live",
+        url: "https://cvmlf.herokuapp.com/live",
+        cache: false
+    })
+    .done(function (html) {
+        $("#live-header").html(html);
+    })
+    .fail(function() {
+      clearInterval(timerId);
+    });
+}
 
